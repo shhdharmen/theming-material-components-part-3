f53e60cfdedfd911e26e18ae38df4367e4db71e8
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-03-24 20:36:29 +0530

    initial commit

---
+++ src/app/custom-material/custom-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+@NgModule({
+  exports: [
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatToolbarModule
+  ]
+})
+export class CustomMaterialModule {}
---
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-sidenav",
+  templateUrl: "./sidenav.component.html",
+  styleUrls: ["./sidenav.component.scss"]
+})
+export class SidenavComponent implements OnInit {
+  @Input() themeColor = "";
+  constructor() {}
+
+  ngOnInit() {}
+}
